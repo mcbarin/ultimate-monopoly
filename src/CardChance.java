059468a2e5aaee394bcd6ca602dbb9e31ac5419d
mcbarin <mcagataybarin@gmail.com>
2015-11-14 18:08:07 +0200

    card classes added

---
+++ src/CardChance.java
@@ -0,0 +1,28 @@
+
+public class CardChance extends Card {
+	int number;
+	String description;
+	Board board;
+
+	public CardChance(int number,String description){
+		this.number = number;
+		this.description = description;
+		this.board = board;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public String[] doAction(Player p){
+		String[] result= new String[20];
+		
+		
+		return result;
+	}
+	
+}
--- src/Player.java
+++ src/Player.java
@@ -2,8 +2,23 @@ import java.awt.List;
 import java.util.ArrayList;
 
 public class Player {
-	private int money,id,position,numberOfProperties=0,numberOfCards=0,valueOfProperties=0;
+	private int money,id,row,position,numberOfProperties=0,numberOfCards=0,valueOfProperties=0,
+			numberOfHouses=0,numberOfHotels=0,numberOfSkyscrapers=0;
+	String name = "";
 	private ArrayList<SquareProperty> properties = new ArrayList<SquareProperty>();
+	private ArrayList<Card> cards = new ArrayList<Card>();
+	
+	
+	public Player(int money, int id, int row, int position, String name) {
+		super();
+		this.money = money;
+		this.id = id;
+		this.row = row;
+		this.position = position;
+		this.name = name;
+	}
+	
+	
 	
 	
 	
