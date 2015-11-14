937c522f2c65bf19f08303ba4ae33eae7bf6a88a
mcbarin <mcagataybarin@gmail.com>
2015-11-14 16:08:22 +0200

    asdasd

--- src/SquareRollOnce.java
+++
@@ -1,9 +0,0 @@
-
-public class SquareRollOnce extends Square {
-
-	public SquareRollOnce(String name) {
-		super(name,"SquareRollOnce",GUI.myWhite);
-		// TODO Auto-generated constructor stub
-	}
-
-}
