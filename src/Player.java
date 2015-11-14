4e83133b8495e8dfc425a94b8330569cce8f3ce7
mcbarin <mcagataybarin@gmail.com>
2015-11-14 16:50:19 +0200

    player class changed

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,10 @@
+import java.awt.List;
+import java.util.ArrayList;
 
 public class Player {
-
+	private int money,id,position,numberOfProperties=0,numberOfCards=0,valueOfProperties=0;
+	private ArrayList<Property> properties = new ArrayList<Property>();
+	
+	
+	
 }
