fd81214d292e132d58df2ca1b038ce19529dff3c
mcbarin <mcagataybarin@gmail.com>
2015-11-14 16:55:26 +0200

    changed

--- src/Player.java
+++ src/Player.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class Player {
 	private int money,id,position,numberOfProperties=0,numberOfCards=0,valueOfProperties=0;
-	private ArrayList<Property> properties = new ArrayList<Property>();
+	private ArrayList<SquareProperty> properties = new ArrayList<SquareProperty>();
 	
 	
 	
--- src/SquareProperty.java
+++ src/SquareProperty.java
@@ -1,4 +1,4 @@
 
-public class Property {
+public class SquareProperty {
 
 }
