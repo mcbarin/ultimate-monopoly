a6ae59b92a782210a27df876a174485401ec39cf
Ezgi Daldal <hdaldal@ku.edu.tr>
2015-11-15 21:15:31 +0200

    no message

--- src/Square.java
+++ src/Square.java
@@ -4,12 +4,12 @@ public abstract class Square {
 	int id;
 	int positionX;
 	int positionY;
-    int row;
-    Square next;
-    Square prev;
-    Square tranNext;
-    
-	
+	int row;
+	Square next;
+	Square prev;
+	Square tranNext;
+
+
 	public Square(String name,int id, int positionX, int positionY, int row) {
 		this.name = name;
 		this.id = id;
@@ -20,41 +20,66 @@ public abstract class Square {
 		//this.prev = prev;
 		//this.tranNext = tranNext;
 	}
-	
+
 	public abstract String[] landOn(Player player, Board board, int total);
 
 	public void initializeResult(String[] result){
 		for (int i = 0; i < result.length; i++) {
 			result[i]= "0";
 		}
-		
+
 	}
-	
+
+
+
+
 	public String[] buyProperty(SquareProperty s,Player p){
 		String[] result = new String[14];
 		initializeResult(result);
-			p.substract(s.price);
-			p.addProperty(s);
-			result[0]="1"; // Success
-			result[1] = name + " has bought the " + ""+s.name+".";
-			result[this.id+2] = "-"+ ""+s.price;
+		p.substract(s.price);
+		p.addProperty(s);
+		result[0]="1"; // Success
+		result[1] = name + " has bought the " + ""+s.name+".";
+		result[this.id+2] = "-"+ ""+s.price;
 		return result;
 	}
-	
-	
+
+
 	public String[] build(SquareProperty s,Player p){
 		String[] result = new String[14];
 		initializeResult(result);
+		int pr = 0;
 		
 		if(s.house<4){
+			result[1]="'"+p.name+"' built house to ";
+			int ids[] = p.board.getOtherProperties(s.color); //get ids of properties of specific color
+			for (int i = 0; i < ids.length; i++) {
+				SquareProperty ss = p.board.getSquareFromBoard(ids[i]); //get SquareProperty object by id
+				if(ss.owner == p){
+					ss.house = ss.house + 1; //build house to all owned properties
+					pr = pr + ss.housePrice;
+					updateRentAccordingToHouse(ss,ss.house);
+					result[1] = result[1] + ss.name + ", ";
+				}
+			}
+			result[0]="1";
+			result[1] = result[1] + "and lost $" +pr;
+			result[p.id+2] = Integer.toString(pr);
+
+		}else if(s.house==4){
 			
 		}
-		
-		
+
+
 		return result;
 	}
+
+
 	
-
-
+	public void updateRentAccordingToHouse(SquareProperty ss, int house){
+		
+		
+		
+	}
 }
 
